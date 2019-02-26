bc20ae1b3fc1f4314f70bd17af3b2187b181f9df
Volodymyr <gunko63@gmail.com>
2019-02-26 11:13:26 -0500

    AutoValue with Builder

---
+++ src/test/java/AutoValue/AutoValueTest.java
@@ -0,0 +1,30 @@
+package AutoValue;
+
+import org.testng.annotations.Test;
+
+public class AutoValueTest {
+    @Test
+    public void testAnimal(){
+        Animal dog = Animal.create("dog", 3);
+        Animal cat = Animal.create("cat", 4);
+        Animal cat2 = Animal.create("cat", 4);
+        System.out.println("dog hashCode: "+dog.hashCode()+" -> "+dog.toString());
+        System.out.println("cat  hashCode: "+cat.hashCode()+" -> "+cat.toString());
+        System.out.println("cat2 hashCode: "+cat2.hashCode()+" -> "+cat2.toString());
+        System.out.println(cat.equals(cat2));
+    }
+
+    @Test
+    public void testMoney(){
+        MoneyWithBuilder grivna =  MoneyWithBuilder.builder()
+                .setAmount(10)
+                .setMoney("GRN")
+                .build();
+        MoneyWithBuilder rubl = MoneyWithBuilder.builder()
+                .setAmount(10)
+                .setMoney("RUB")
+                .build();
+        System.out.println("GRN == RUB : "+grivna.equals(rubl));
+        System.out.println(grivna.toString());
+    }
+}
