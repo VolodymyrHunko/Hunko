c48a7580f19082442716b1b2e6c5d14b45f09c7f
volo <gunko63@gmail.com>
2019-03-06 21:50:09 -0500

    enum added

--- src/test/java/coreJava/EnumExample.java
+++ src/test/java/coreJava/EnumExample.java
@@ -3,8 +3,8 @@ package coreJava;
 import org.testng.annotations.Test;
 
 public class EnumExample {
-
-    enum parameterINT{  //enum defined with parameters (can avoid it)
+    //enum defined with parameters (can avoid it).................................................
+    enum parameterINT {
         PENNY(1),
         NICKLE(5),
         DIME(10),
@@ -12,20 +12,57 @@ public class EnumExample {
 
         private int argument; //var for keeping value of argument
 
-        parameterINT(int argument){ //constructor to initialize var argument by passing constant
+        parameterINT(int argument) { //constructor to initialize var argument by passing constant
             this.argument = argument;
         }
 
-        public int getArgument(){ //method to return argument
+        public int getArgument() { //method to return argument
             return argument;
         }
     }
 
     @Test
     void testEnum() {
-        System.out.println("Parameter can be send as argument: "+parameterINT.PENNY.getArgument());
+        System.out.println("Parameter can be send as argument: " + parameterINT.PENNY.getArgument()); // 1
         parameterINT ar = parameterINT.valueOf("DIME");
-        System.out.println("Value can be send as argument: "+ ar);
+        System.out.println("Value can be send as argument: " + ar); //DIME
     }
+
+
+    //example to pass enum to class.............................................................
+    enum rank {
+        JACK,
+        QUIN,
+        KING,
+        ASS;
+    }
+
+    enum suit {
+        DIAMOND,
+        CLUB,
+        HARD,
+        SPADE;
+    }
+
+    private class Cards {
+        private suit mySuit;
+        private rank myRank;
+
+        private Cards(rank r, suit s) {
+            this.mySuit = s;
+            this.myRank = r;
+        }
+    }
+
+    @Test
+    void testCard() {
+        assert (rank.JACK == rank.valueOf("JACK"));
+        assert (suit.CLUB == suit.valueOf("CLUB"));
+        Cards myCard = new Cards(rank.ASS, suit.HARD);
+        System.out.println("My card: " + myCard.myRank + ":" + myCard.mySuit);
+        parameterINT ar = EnumExample.parameterINT.valueOf("DIME");
+        System.out.println("Value can be send as argument: " + ar); //DIME
+    }
+
 }
 
