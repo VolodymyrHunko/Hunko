845dbfd9b9a47643c1f6c101cfa8ef0b4fc399c7
Volodymyr <gunko63@gmail.com>
2019-03-08 16:16:50 -0500

    inner class updated

--- src/test/java/coreJava/AnonymousClass.java
+++ src/test/java/coreJava/AnonymousClass.java
@@ -2,24 +2,31 @@ package coreJava;
 
 public class AnonymousClass { //does not implement interface
     private static String str = "Main class string";
-    //it is a regular implementation of external class as member class (for comparison)
-    private B abc = new B(111);
+    //it is a regular implementation of inner ClASS as external class  (for comparison)
+    // diff - no body
+    final static private B abc = new B(111);
+
     public static void main (String [] args){
         // anonymous class from A interface
         // same as local class, but without name (uses the name of parent call)
         // skipp an implementation of interface
         // only ONE interface can implement (real class can multiply)
         // has access to outer var
-        // can be replaced with lambda
+        // can be replaced with lambda if only one method has implemented interface.
         A a = new A() {
             @Override
             public void output() { //must override all methods from interface
                 System.out.println("anonymous of A class + "+ str);
             }
+
+            @Override
+            public void setVar() {
+                abc.output(); // have access to final static fields of outer class
+            }
         };
         a.output();
 
-        // anonymous class from B real class
+        // anonymous local class from B real class
         // CANNOT have constructor (no name)
         B b = new B(333){
             // added some extra functionality, CANNOT be static!!!
@@ -41,6 +48,7 @@ public class AnonymousClass { //does not implement interface
 //interface can be real Java, such sa Runnable()
 interface A{
     void output();
+    void setVar();
 }
 
 //class can be a real Java, such as Thread()
