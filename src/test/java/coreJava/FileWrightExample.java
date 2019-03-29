eeba8a2321748ab9cf48542bb6c72069692cc438
Volodymyr <gunko63@gmail.com>
2019-03-29 16:49:27 -0400

    io updated

--- src/test/java/coreJava/FileWrightExample.java
+++ src/test/java/coreJava/FileWrightExample.java
@@ -1,14 +1,23 @@
 package coreJava;
 
+import org.testng.annotations.Test;
+
 import java.io.*;
 
 public class FileWrightExample {
-    public static void main(String[] args) {
-        FileOutputStream fos;
-        BufferedWriter bw;
-        File file;
-        File file2;
-        String myContent = "This is my Data which needs to be written into the file";
+    FileOutputStream fos;
+    BufferedWriter bw;
+    File file;
+    File file2;
+    String myContent = "This is my Data which needs to be written into the file";
+
+    /*
+     * first method by using File Output Stream
+     * String content cannot be directly written into
+     * a file. It needs to be converted into bytes
+     */
+    void firstMethod() {
+
         try {
             //Specify the file path here
             file = new File("/home/volodymyr/IdeaProjects/Hunko/src/test/resources/myFile.txt");
@@ -18,55 +27,82 @@ public class FileWrightExample {
              * exists or not. If the file is not found
              * at the specified location it would create
              * a new file
-             * */
-            if (!file.exists()) {
+             */
+            if (file.exists()) {
+                System.out.println("File exists");
+            } else {
+                System.out.println("File created");
                 file.createNewFile();
             }
-            /*
-             * first method by using File Output Stream
-             * String content cannot be directly written into
-             * a file. It needs to be converted into bytes
-             */
+
             fos = new FileOutputStream(file);
             byte[] bytesArray = myContent.getBytes();
             fos.write(bytesArray);
             fos.flush();
             fos.close();
             System.out.println("File Written Successfully");
-
-            //Here true is to append the content to file
-            FileWriter fw1 = new FileWriter(file,true);
-            BufferedWriter bw1 = new BufferedWriter(fw1);
-            PrintWriter pw = new PrintWriter(bw1);
-            //This will add a new line to the file content
-            pw.println("\n");
-
-            /* Below three statements would add three
-             * mentioned Strings to the file in new lines.
-             */
-            pw.println("This is first line");
-            pw.println("This is the second line");
-            pw.println("This is third line");
-            pw.close();
-
-            /*
-             *Second method by using FileWriter and append content to the file
-             */
-            FileWriter fw = new FileWriter(file2);
-            bw = new BufferedWriter(fw);
-            bw.write(myContent);
-            bw.close();
-            System.out.println("File 2 written Successfully");
-            //Here true is to append the content to file
-            FileWriter fw2 = new FileWriter(file2,true);
-            //BufferedWriter writer give better performance
-            BufferedWriter bw2 = new BufferedWriter(fw2);
-            bw2.write("\n"+myContent+" one more time...");
-            //Closing BufferedWriter Stream
-            bw2.close();
-            System.out.println("Data successfully appended at the end of file");
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    /*
+     *Second method by using FileWriter and append content to the file
+     */
+    void secondMethod() throws IOException {
+        FileWriter fw = new FileWriter(file2);
+        bw = new BufferedWriter(fw);
+        bw.write(myContent);
+        bw.close();
+        System.out.println("File 2 written Successfully");
+        //Here true is to append the content to file
+        FileWriter fw2 = new FileWriter(file2, true);
+        //BufferedWriter writer give better performance
+        BufferedWriter bw2 = new BufferedWriter(fw2);
+        bw2.write("\n" + myContent + " one more time...");
+        //Closing BufferedWriter Stream
+        bw2.close();
+        System.out.println("Data successfully appended at the end of file");
+    }
+
+    /*
+     * Method to apend data to existing file
+     */
+    void appendFileDta() throws IOException {
+        //Here 'true' is to append the content to file
+        FileWriter fw1 = new FileWriter(file, true);
+        BufferedWriter bw1 = new BufferedWriter(fw1);
+        PrintWriter pw = new PrintWriter(bw1);
+        //This will add a new line to the file content
+        pw.println("\n");
+
+        /* Below three statements would add three
+         * mentioned Strings to the file in new lines.
+         */
+        pw.println("This is first line");
+        pw.println("This is the second line");
+        pw.println("This is third line");
+        pw.close();
+
+    }
+
+    @Test
+    void testFirstwriting() {
+        firstMethod();
+    }
+
+    @Test
+    void testSecondwriting() throws IOException {
+        secondMethod();
+    }
+
+    @Test
+    void testAppendFile() throws IOException {
+        appendFileDta();
+    }
+
+    @Test
+    void testSerialization(){
+
     }
 }
